3b64dcb0df36fcbf0aa8ba8fd8ed926f2b1706fc
Richard Jones <richard@cottagelabs.com>
2013-02-28 23:42:03 +0000

    update to use latest version of gtr datamodel

--- gtr.py
+++ gtr.py
@@ -19,11 +19,11 @@ def get_fund(identifier):
 """
 
 def get_org(identifier):
-    raw = get("http://gtr.rcuk.ac.uk/orgunit/" + identifier)
+    raw = get("http://gtr.rcuk.ac.uk/organisation/" + identifier)
     return Org(raw)
     
 def get_person(identifier):
-    raw = get("http://gtr.rcuk.ac.uk/pers/" + identifier)
+    raw = get("http://gtr.rcuk.ac.uk/person/" + identifier)
     return Person(raw)
 
 def get_publication(identifier):
@@ -35,7 +35,7 @@ class Native(object):
         self.raw = raw
     
     def from_xpath(self, xp):
-        els = self.raw.xpath(xp)
+        els = self.raw.xpath(xp, namespaces={"gtr" : "http://gtr.rcuk.ac.uk/api"})
         if els is not None and len(els) > 0:
             return els[0].text
         return None
@@ -45,15 +45,15 @@ class Native(object):
 
 class Person(Native):
     def id(self):
-        return self.from_xpath("/personOverview/person/id")
+        return self.from_xpath("/gtr:personOverview/gtr:person/gtr:id")
         
     def name(self):
-        return self.from_xpath("/personOverview/person/name")
+        return self.from_xpath("/gtr:personOverview/gtr:person/gtr:name")
             
     def projects(self):
         projects = []
-        for el in self.raw.xpath("/personOverview/projectOverviews"):
-            root = etree.Element("projectOverview")
+        for el in self.raw.xpath("/gtr:personOverview/gtr:projectOverviews", namespaces={"gtr" : "http://gtr.rcuk.ac.uk/api"}):
+            root = etree.Element("{http://gtr.rcuk.ac.uk/api}projectOverview")
             for child in el:
                 root.append(deepcopy(child))
             projects.append(Project(root))
@@ -61,71 +61,70 @@ class Person(Native):
 
 class Org(Native):
     def id(self):
-        return self.from_xpath("/organisationOverview/organisation/id")
+        return self.from_xpath("/gtr:organisationOverview/gtr:organisation/gtr:id")
         
     def name(self):
-        return self.from_xpath("/organisationOverview/organisation/name")
+        return self.from_xpath("/gtr:organisationOverview/gtr:organisation/gtr:name")
         
 
 class Project(Native):
     def id(self):
-        return self.from_xpath("/projectOverview/project/id")
+        return self.from_xpath("/gtr:projectOverview/gtr:projectComposition/gtr:project/gtr:id")
 
     def title(self):
-        return self.from_xpath("/projectOverview/project/title")
+        return self.from_xpath("/gtr:projectOverview/gtr:projectComposition/gtr:project/gtr:title")
     
     def start(self):
-        return self.from_xpath("/projectOverview/project/fund/start")
+        return self.from_xpath("/gtr:projectOverview/gtr:projectComposition/gtr:project/gtr:fund/gtr:start")
         
     def end(self):
-        return self.from_xpath("/projectOverview/project/fund/end")
+        return self.from_xpath("/gtr:projectOverview/gtr:projectComposition/gtr:project/gtr:fund/gtr:end")
     
     def abstract(self):
-        return self.from_xpath("/projectOverview/project/abstractText")
+        return self.from_xpath("/gtr:projectOverview/gtr:projectComposition/gtr:project/gtr:abstractText")
     
     def funder(self):
-        return self.from_xpath("/projectOverview/project/fund/funder/name")
+        return self.from_xpath("/gtr:projectOverview/gtr:projectComposition/gtr:project/gtr:fund/gtr:funder/gtr:name")
     
     def value(self):
-        return self.from_xpath("/projectOverview/project/fund/value")
+        return self.from_xpath("/gtr:projectOverview/gtr:projectComposition/gtr:project/gtr:fund/gtr:valuePounds")
     
     def category(self):
-        return self.from_xpath("/projectOverview/project/grantCategory")
+        return self.from_xpath("/gtr:projectOverview/gtr:projectComposition/gtr:project/gtr:grantCategory")
     
     def reference(self):
-        return self.from_xpath("/projectOverview/project/grantReference")
+        return self.from_xpath("/gtr:projectOverview/gtr:projectComposition/gtr:project/gtr:grantReference")
+    
+    def _get_id_name(self, parent_xpath):
+        os = []
+        for org in self.raw.xpath(parent_xpath, namespaces={"gtr" : "http://gtr.rcuk.ac.uk/api"}):
+            id_el = org.xpath("gtr:id", namespaces={"gtr" : "http://gtr.rcuk.ac.uk/api"})
+            id = None
+            if id_el is not None and len(id_el) > 0:
+                id = id_el[0].text
+            name_el = org.xpath("gtr:name", namespaces={"gtr" : "http://gtr.rcuk.ac.uk/api"})
+            name = None
+            if id_el is not None and len(name_el) > 0:
+                name = name_el[0].text
+            os.append((id, name))
+        return os
     
     def lead(self):
-        os = []
-        for org in self.raw.xpath("/projectOverview/leadResearchOrganisation"):
-            id = org.xpath("id")[0].text
-            name = org.xpath("name")[0].text
-            os.append((id, name))
-        return os
+        return self._get_id_name("/gtr:projectOverview/gtr:projectComposition/gtr:leadResearchOrganisation")
         
     def orgs(self):
-        os = []
-        for org in self.raw.xpath("/projectOverview/organisations"):
-            id = org.xpath("id")[0].text
-            name = org.xpath("name")[0].text
-            os.append((id, name))
-        return os
+        return self._get_id_name("/gtr:projectOverview/gtr:projectComposition/gtr:organisations/gtr:organisation")
         
     def people(self):
-        os = []
-        for pers in self.raw.xpath("/projectOverview/coInvestigators"):
-            id = pers.xpath("id")[0].text
-            name = pers.xpath("name")[0].text
-            os.append((id, name))
-        for pers in self.raw.xpath("/projectOverview/principalInvestigators"):
-            id = pers.xpath("id")[0].text
-            name = pers.xpath("name")[0].text
-            os.append((id, name))
-        return os
+        os = self._get_id_name("/gtr:projectOverview/gtr:projectComposition/gtr:projectPeople/gtr:projectPerson")
+        # os2 = self._get_id_name("/gtr:projectOverview/gtr:projectComposition/gtr:projectPeople/gtr:principalInvestigators")
+        return os # + os2
         
     def outputs(self):
         os = []
-        out = self.raw.xpath("/projectOverview/project/output")
+        out = self.raw.xpath("/gtr:projectOverview/gtr:projectComposition/gtr:project/output", namespaces={"gtr" : "http://gtr.rcuk.ac.uk/api"})
+        if out is None or len(out) == 0: 
+            return os
         for child in out[0]:
             os.append(etree.tostring(child))
         return os
